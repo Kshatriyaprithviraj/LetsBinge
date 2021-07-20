85c7b3d43873209207863920580d242058fecc5f
kshatriyaprithviraj <1999prithviraj1999@gmail.com>
2021-07-20 18:21:49 +0530

    finalized messy directory structure

--- LetsBinge/wsgi.py
+++ LetsBinge/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for letsbinge project.
+WSGI config for LetsBinge project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letsbinge.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LetsBinge.settings')
 
 application = get_wsgi_application()
